85a53b828094e6e9d3331b07fc14596e318b2ce8
andrey <a.cheremisin@greenlabs.ru>
2023-01-28 20:16:18 +0300

    update liker to new design

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-url = 'https://opensea.io/'
+url = 'https://opensea.io'
 
 
 def get_options():
@@ -15,50 +15,68 @@ def get_options():
     options.add_argument("--enable-file-cookies")
     options.add_argument(
         "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
-    options.add_experimental_option('excludeSwitches', ['enable-logging', 'enable-automation'])
-    options.add_experimental_option('useAutomationExtension', False)
-    options.add_argument("--user-data-dir=/home/andr01d/.config/google-chrome")
+    # options.add_experimental_option('excludeSwitches', ['enable-logging', 'enable-automation'])
+    # options.add_experimental_option('useAutomationExtension', False)
+    # options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--user-data-dir=/home/andrey/.config/google-chrome")
     return options
 
 
+def open_and_switch_new_tab(driver, url):
+    driver.execute_script(f"window.open('{url}')")
+    driver.switch_to.window(driver.window_handles[1])
+
+
+def get_links(driver):
+    cards = WebDriverWait(driver, 15).until(EC.visibility_of_all_elements_located(
+        (By.CSS_SELECTOR, 'a.Asset--anchor')))
+    return [card.get_attribute('href') for card in cards]
+
+
 if __name__ == '__main__':
-    amount_likes = int(input('Введите количество лайков'))
-    # amount_likes = 100
+    # amount_likes = int(input('Введите количество лайков'))
+    amount_likes = 35
 
     driver = webdriver.Chrome(options=get_options(),
                               executable_path="/usr/local/bin/chromedriver")
-    driver.get(f'{url}login?referrer=%2Faccount')
 
-    # todo Передать действие пользователю
+    time.sleep(1)
+    driver.get(f'{url}/login?referrer=%2Faccount')
 
-    for i in range(30):
+    for i in range(10):
         time.sleep(1)
-        if i % 10 == 0:
-            print(f"Осталось {30 - i} сек...")
+        if i % 5 == 0:
+            print(f"Осталось {10 - i} сек...")
 
     print('Контроль возвращается боту')
-    driver.get(f'{url}assets')
+    driver.get(f'{url}/assets')
     amount_errors = 0
-    liked_element = 1
+    handled_element = 1
     scroll_pixels = 6000
     while True:
         try:
-            buttons = WebDriverWait(driver, 15).until(EC.visibility_of_all_elements_located(
-                (By.CSS_SELECTOR, 'div[role=gridcell] article footer div:nth-child(2) button')))
+            links = get_links(driver)
         except selenium.common.exceptions.TimeoutException:
-            driver.get(f'{url}assets')
+            driver.get(f'{url}/assets')
             continue
 
-        for button in buttons:
+        for link in links:
+            open_and_switch_new_tab(driver, link)
             try:
-                print(f'like №{liked_element}')
+                print(f'like №{handled_element}')
+                button = WebDriverWait(driver, 15).until(EC.visibility_of_element_located(
+                    (By.CSS_SELECTOR, '.item--wrapper article header div:nth-child(3) button')))
                 time.sleep(0.5)
                 driver.execute_script("arguments[0].click();", button)
-                liked_element += 1
-            except:
+                time.sleep(0.5)
+            except Exception as err:
                 amount_errors += 1
+                print(err)
                 print(f'Ошибка №{amount_errors} идем дальше')
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
+            handled_element += 1
         driver.execute_script(f"window.scrollTo(0, {scroll_pixels});")
         scroll_pixels += 6000
-        if liked_element >= amount_likes:
+        if handled_element >= amount_likes:
             break
